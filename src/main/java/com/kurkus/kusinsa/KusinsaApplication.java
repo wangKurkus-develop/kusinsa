b80ca413ea2ae383e3bcbf0c9a9953706428847d
cwangg897 <dhkdrb897@gmail.com>
2022-12-20 15:04:03 +0900

    [#1] 프로젝트 생성

---
+++ src/main/java/com/kurkus/kusinsa/KusinsaApplication.java
@@ -0,0 +1,13 @@
+package com.kurkus.kusinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusinsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KusinsaApplication.class, args);
+    }
+
+}

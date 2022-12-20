919cb783e269553393a3fdb2c99b2cc95f590bf7
Choi Wang Gyu <79621675+cwangg897@users.noreply.github.com>
2022-12-20 15:05:06 +0900

    [#1] 프로젝트 생성 (#2)

---
+++ src/test/java/com/kurkus/kusinsa/KusinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurkus.kusinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusinsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

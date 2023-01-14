4062b275c4a953a897dcd5e9be65b3d1f6f18830
cwangg897 <dhkdrb897@gmail.com>
2023-01-14 21:19:09 +0900

    feat : 로그인시 포인트 지급을 위한 event

---
+++ src/main/java/com/kurkus/kusinsa/events/point/PointEventHandler.java
@@ -0,0 +1,34 @@
+package com.kurkus.kusinsa.events.point;
+
+import javax.transaction.Transactional;
+
+import com.kurkus.kusinsa.dto.request.point.PointCreateRequest;
+import com.kurkus.kusinsa.enums.PointType;
+import com.kurkus.kusinsa.service.point.PointService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PointEventHandler {
+
+    private final PointService pointService;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(Transactional.TxType.REQUIRES_NEW)
+    public void pointSave(PointSavedEvent event) {
+        PointCreateRequest request = PointCreateRequest.builder()
+                .score(event.getScore())
+                .division(PointType.OBTAIN)
+                .content(event.getContent())
+                .build();
+
+        pointService.save(event.getUserId(), request);
+        log.info("{} 포인트 저장 완료", event.getUserId());
+    }
+}
---
+++ src/main/java/com/kurkus/kusinsa/utils/constants/PointMessages.java
@@ -0,0 +1,8 @@
+package com.kurkus.kusinsa.utils.constants;
+
+public final class PointMessages {
+
+    // 포인트
+    public static final Long LOGIN_POINT = 100L;
+    public static final String LOGIN_POINT_CONTENT = "로그인 포인트";
+}

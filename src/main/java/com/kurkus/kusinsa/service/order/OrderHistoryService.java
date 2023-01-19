cd659e7cbc2dfdf2a85fe61c2d67f28d9575c250
cwangg897 <dhkdrb897@gmail.com>
2023-01-19 18:59:05 +0900

    feat : 주문취소

--- src/main/java/com/kurkus/kusinsa/service/order/OrderHistoryService.java
+++ src/main/java/com/kurkus/kusinsa/service/order/OrderHistoryService.java
@@ -4,7 +4,11 @@ package com.kurkus.kusinsa.service.order;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.kurkus.kusinsa.utils.constants.PageSizeConstants.HISTORY_SIZE;
+import static com.kurkus.kusinsa.utils.constants.PageSizeConstants.POINT_SIZE;
+
 import com.kurkus.kusinsa.dto.request.order.OrderProductRequest;
+import com.kurkus.kusinsa.dto.response.orderhistory.OrderHistoryResponse;
 import com.kurkus.kusinsa.entity.Order;
 import com.kurkus.kusinsa.entity.OrderHistory;
 import com.kurkus.kusinsa.entity.Product;
@@ -12,6 +16,8 @@ import com.kurkus.kusinsa.entity.User;
 import com.kurkus.kusinsa.repository.OrderHistoryRepository;
 import com.kurkus.kusinsa.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,13 +27,31 @@ public class OrderHistoryService {
 
 
     private final ProductRepository productRepository;
-    private final OrderHistoryRepository orderHistoryRepository;
+    private final OrderHistoryRepository historyRepository;
 
     @Transactional
     public void save(Order order, OrderProductRequest request){
         Product product = productRepository.getById(request.getProductId());
         OrderHistory orderHistory = request.toOrderHistory(order, product);
-        orderHistoryRepository.save(orderHistory);
+        historyRepository.save(orderHistory);
     }
 
+    @Transactional(readOnly = true)
+    public Page<OrderHistoryResponse> findAllWithPage(Long userId, int page) {
+        Page<OrderHistory> result = historyRepository.findAllWithPage(userId, PageRequest.of(page, HISTORY_SIZE));
+        return result.map(h -> OrderHistoryResponse.from(h));
+    }
+
+    /**
+     * 주문 취소는 배송준비중일떄만 가능하다
+     * 주문취소는 수량개수도 다시 돌려놓아야하고 SOLD OUT이면 상태를 변경시켜야함
+     * 주문 취소는 Lock걸어야한다 주문취소할때 배송출발으로 바뀌면 안되기때문이다.
+     * 이에따른 포인트도 회수해야함
+     * 기록을 해야지 취소기록을 (이런거는 로그로해야하나보구나)
+     */
+    @Transactional
+    public void cancel(Long userId, Long historyId) {
+        OrderHistory history = historyRepository.getById(historyId);
+
+    }
 }

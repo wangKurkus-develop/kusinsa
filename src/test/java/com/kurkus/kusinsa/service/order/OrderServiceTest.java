841bd1efed1b433a25edd85075f295542a92d586
cwangg897 <dhkdrb897@gmail.com>
2023-01-20 21:49:30 +0900

    test : orderService만

--- src/test/java/com/kurkus/kusinsa/service/order/OrderServiceTest.java
+++ src/test/java/com/kurkus/kusinsa/service/order/OrderServiceTest.java
@@ -1,4 +1,98 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.kurkus.kusinsa.service.order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.kurkus.kusinsa.dto.request.order.OrderCreateRequest;
+import com.kurkus.kusinsa.dto.request.order.OrderProductRequest;
+import com.kurkus.kusinsa.entity.Order;
+import com.kurkus.kusinsa.entity.Product;
+import com.kurkus.kusinsa.entity.User;
+import com.kurkus.kusinsa.enums.ProductType;
+import com.kurkus.kusinsa.events.order.OrderHistorySavedEvent;
+import com.kurkus.kusinsa.events.point.PointOrderSavedEvent;
+import com.kurkus.kusinsa.repository.OrderRepository;
+import com.kurkus.kusinsa.repository.ProductRepository;
+import com.kurkus.kusinsa.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.mockito.BDDMockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
-  
+    
+    @Spy
+    @InjectMocks
+    OrderService orderService;
+
+    @Mock
+    OrderRepository orderRepository;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    ProductRepository productRepository;
+
+    @Mock
+    ApplicationEventPublisher publisher;
+
+    @Test
+    public void save() {
+        // given
+        Long productId = 5L;
+        Long userId = 18L;
+
+        List<OrderProductRequest> requestList = new ArrayList<>();
+        OrderProductRequest orderProduct = OrderProductRequest.builder()
+                .productId(productId)
+                .obtainPoint(500)
+                .quantity(2)
+                .price(3000L)
+                .build();
+        requestList.add(orderProduct);
+
+        OrderCreateRequest orderCreateRequest = OrderCreateRequest.builder()
+                .orderPrice(10000L)
+                .totalObtainPoint(500)
+                .totalUsedPoint(5000)
+                .orderProductRequestList(requestList)
+                .deliveryAddress("배송지2")
+                .build();
+
+        given(productRepository.findByIdWithPessimisticLock(productId)).willReturn(Product.builder().
+                stock(10).status(ProductType.SALE).build());
+        given(userRepository.getById(anyLong())).willReturn(User.builder().id(userId).build());
+        given(orderRepository.save(any())).willReturn(Order.builder().totalObtainPoint(10).totalUsedPoint(10).build());
+        // when
+        orderService.save(userId, orderCreateRequest);
+        // then
+        then(orderRepository).should(times(1)).save(any());
+        then(orderService).should(times(1)).decrease(anyLong(), anyInt());
+    }
+
+    @Test
+    public void decrease() {
+        // given
+        Long productId = 10L;
+        int quantity = 10;
+        given(productRepository.findByIdWithPessimisticLock(any())).
+                willReturn(Product.builder().stock(20).status(ProductType.SALE).build());
+        // when
+        orderService.decrease(productId, quantity);
+        // then
+        then(productRepository).should(times(1)).flush();
+    }
+
+
+
 }
--- src/test/java/com/kurkus/kusinsa/test.java
+++ src/test/java/com/kurkus/kusinsa/test.java
@@ -1,2 +1,60 @@
-package com.kurkus.kusinsa;public class test {
+package com.kurkus.kusinsa;
+
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.kurkus.kusinsa.entity.Brand;
+import com.kurkus.kusinsa.entity.Category;
+import com.kurkus.kusinsa.entity.OrderHistory;
+import com.kurkus.kusinsa.entity.Product;
+import com.kurkus.kusinsa.repository.OrderHistoryRepository;
+import com.kurkus.kusinsa.repository.ProductRepository;
+import com.kurkus.kusinsa.service.order.OrderHistoryService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class test {
+
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    OrderHistoryService orderHistoryService;
+
+    @Autowired
+    OrderHistoryRepository historyRepository;
+
+    @Test
+    public void joinfecth() throws Exception {
+        // given
+
+
+        // then
+    }
+
+
+
+
+    @Test
+    @Transactional
+    public void test() throws Exception {
+        // given
+        for (long i = 11; i <= 20; i++) {
+            System.out.println(i + ": 시작 ");
+            decrease(i);
+            decrease(11L);
+        }
+    }
+
+    @Test
+    @Transactional
+    public void decrease(Long id) throws Exception {
+        // given 1번과 5번
+        productRepository.findByIdWithPessimisticLock(id);
+        Thread.sleep(1000);
+    }
 }

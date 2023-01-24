ee2728fe72654525b500a95fbe03d32abdd100d9
cwangg897 <dhkdrb897@gmail.com>
2023-01-24 20:01:34 +0900

    test : cart integrationtest

--- src/test/java/com/kurkus/kusinsa/test.java
+++ src/test/java/com/kurkus/kusinsa/test.java
@@ -1,10 +1,14 @@
 package com.kurkus.kusinsa;
 
 
+import java.sql.Timestamp;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.kurkus.kusinsa.dao.CountDao;
+import com.kurkus.kusinsa.dao.RecentDao;
 import com.kurkus.kusinsa.entity.Brand;
 import com.kurkus.kusinsa.entity.Category;
 import com.kurkus.kusinsa.entity.OrderHistory;
@@ -13,23 +17,47 @@ import com.kurkus.kusinsa.repository.OrderHistoryRepository;
 import com.kurkus.kusinsa.repository.ProductRepository;
 import com.kurkus.kusinsa.service.ProductService;
 import com.kurkus.kusinsa.service.order.OrderHistoryService;
+import oracle.sql.TIMESTAMP;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
-
 
 @SpringBootTest
 public class test {
 
     @Autowired
-    ProductService productService;
+    CountDao countDao;
+
+    @Autowired
+    RedisTemplate<String, Object> redisSetTemplate;
+
+//    @BeforeEach
+//    void setUp(){
+//        redisSetTemplate.opsForZSet().add("click_product",2,0);
+//    }
 
     @Test
-    public void test() throws Exception {
+    public void 동시에_100명이_클릭() throws Exception {
         // given
-        productService.like(3L, 2L);
-        // then
+        int threadCount = 1000;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(40);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    countDao.findCount(2L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
     }
 
 

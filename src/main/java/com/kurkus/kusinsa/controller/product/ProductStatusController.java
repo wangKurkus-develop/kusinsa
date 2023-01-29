101b85b1f7c518b778bde68b7fbdb636055252c5
cwangg897 <dhkdrb897@gmail.com>
2023-01-29 15:40:23 +0900

    feat : 상품 재고 변경

--- src/main/java/com/kurkus/kusinsa/controller/product/ProductStatusController.java
+++ src/main/java/com/kurkus/kusinsa/controller/product/ProductStatusController.java
@@ -1,4 +1,33 @@
 package com.kurkus.kusinsa.controller.product;
 
-public class ProductStockController {
+import com.kurkus.kusinsa.annotation.LoginCheck;
+import com.kurkus.kusinsa.dto.request.product.status.ProductStockUpdateRequest;
+import com.kurkus.kusinsa.enums.UserType;
+import com.kurkus.kusinsa.service.product.ProductStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 재고와 배송상태륿 변경하는 곳을 따로 만들었습니다.
+ *
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products/status")
+public class ProductStatusController {
+
+    private final ProductStatusService productStatusService;
+
+    // 재고변경 (재입고)
+    // 상품번호, 개수가 끝일듯
+    @PatchMapping("/stock")
+    @LoginCheck(userType = UserType.ADMIN)
+    public ResponseEntity<Void> updateStock(@RequestBody ProductStockUpdateRequest request){
+        productStatusService.updateStock(request);
+        return ResponseEntity.ok().build();
+    }
+
+    // 판매중지나 이런거 MVP후에 추가하기
+
 }
--- src/main/java/com/kurkus/kusinsa/dto/request/product/status/ProductStockUpdateRequest.java
+++ src/main/java/com/kurkus/kusinsa/dto/request/product/status/ProductStockUpdateRequest.java
@@ -1,2 +1,15 @@
-package com.kurkus.kusinsa.dto.request.product;public class ProductStockUpdateRequest {
+package com.kurkus.kusinsa.dto.request.product.status;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductStockUpdateRequest {
+
+    private Long productId;
+    private long stock;
 }

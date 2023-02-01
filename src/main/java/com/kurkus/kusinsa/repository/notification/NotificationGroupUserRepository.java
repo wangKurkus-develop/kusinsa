2dc28795c6e6b9cf5bc66a83c9c5a76f030d4b5b
cwangg897 <dhkdrb897@gmail.com>
2023-02-01 17:26:23 +0900

    feat : findBy 커스텀

--- src/main/java/com/kurkus/kusinsa/repository/notification/NotificationGroupUserRepository.java
+++ src/main/java/com/kurkus/kusinsa/repository/notification/NotificationGroupUserRepository.java
@@ -1,7 +1,17 @@
 package com.kurkus.kusinsa.repository.notification;
 
+import java.util.List;
+
 import com.kurkus.kusinsa.entity.notification.NotificationUser;
+import com.kurkus.kusinsa.enums.notification.NotificationGroupStatus;
+import com.kurkus.kusinsa.enums.notification.NotificationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NotificationGroupUserRepository extends JpaRepository<NotificationUser, Long> {
+
+    @Query(value = "select u from NotificationUser u where u.deleted = false and u.status = :status")
+    List<NotificationUser> findAllByGroupId(@Param("groupId") Long groupId, @Param("status") NotificationStatus status);
+
 }

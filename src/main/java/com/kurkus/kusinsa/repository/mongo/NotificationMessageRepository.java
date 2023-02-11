6d30f527962b491bdd1de417df8ffaefe8e8fb62
cwangg897 <dhkdrb897@gmail.com>
2023-02-11 15:58:43 +0900

    feat : doucumnet 추가

--- src/main/java/com/kurkus/kusinsa/repository/mongo/NotificationMessageRepository.java
+++ src/main/java/com/kurkus/kusinsa/repository/mongo/NotificationMessageRepository.java
@@ -1,2 +1,8 @@
-package com.kurkus.kusinsa.repository.mongo;public interface NotificationMessageRepository {
+package com.kurkus.kusinsa.repository.mongo;
+
+import com.kurkus.kusinsa.entity.documents.NotificationMessage;
+import com.kurkus.kusinsa.entity.documents.OrderRank;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NotificationMessageRepository extends MongoRepository<NotificationMessage, String> {
 }

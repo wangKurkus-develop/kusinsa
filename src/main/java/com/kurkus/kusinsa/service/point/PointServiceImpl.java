dff70d217573fad841b88715f772b335f5dcf6c2
cwangg897 <dhkdrb897@gmail.com>
2023-02-12 16:06:12 +0900

    refactor : event 코드 리펙토링

--- src/main/java/com/kurkus/kusinsa/service/point/PointServiceImpl.java
+++ src/main/java/com/kurkus/kusinsa/service/point/PointServiceImpl.java
@@ -1,12 +1,19 @@
 package com.kurkus.kusinsa.service.point;
 
+import static com.kurkus.kusinsa.enums.PointType.OBTAIN;
+import static com.kurkus.kusinsa.enums.PointType.USED;
 import static com.kurkus.kusinsa.utils.constants.PageSizeConstants.*;
+import static com.kurkus.kusinsa.utils.constants.PointMessages.*;
+import static com.kurkus.kusinsa.utils.constants.PointMessages.ORDER_OBTAIN_CONTENT;
 
+import com.kurkus.kusinsa.dao.PointDao;
 import com.kurkus.kusinsa.dto.request.point.PointCreateRequest;
 import com.kurkus.kusinsa.dto.request.point.PointSearchCondition;
 import com.kurkus.kusinsa.dto.response.point.PointResponse;
 import com.kurkus.kusinsa.entity.User;
 import com.kurkus.kusinsa.enums.PointType;
+import com.kurkus.kusinsa.events.point.PointLoginSavedEvent;
+import com.kurkus.kusinsa.events.point.PointOrderSavedEvent;
 import com.kurkus.kusinsa.repository.point.PointRepository;
 import com.kurkus.kusinsa.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,16 +22,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionalProxy;
 
 
 @RequiredArgsConstructor
 @Service
 @Slf4j
-public class PointServiceImpl implements PointService{
+public class PointServiceImpl implements PointService {
 
     private final UserRepository userRepository;
     private final PointRepository pointRepository;
-
+    private final PointDao pointDao;
 
     @Override
     @Transactional
@@ -33,6 +41,29 @@ public class PointServiceImpl implements PointService{
         pointRepository.save(request.toPoint(user));
     }
 
+    @Override
+    @Transactional
+    public void loginPointSave(PointLoginSavedEvent event) {
+        if (pointDao.checkTodayLoginPoint(event.getUserId()) == 1) {
+            PointCreateRequest request = new PointCreateRequest(LOGIN_POINT, LOGIN_POINT_CONTENT, OBTAIN);
+            save(event.getUserId(), request);
+        }
+    }
+
+    // 적립된 포인트, 내용(상품 결제),
+    // 사용 포인트가 0이면 획득한것만 저장, 아니면은 사용한것 저장 + 획득한것
+    @Transactional
+    public void orderPointSave(PointOrderSavedEvent event) {
+        if (event.getUsedPoint() != 0) {
+            save(event.getUserId(),
+                    new PointCreateRequest(event.getUsedPoint(),
+                            ORDER_USED_CONTENT + event.getOrderId(), USED));
+        }
+        save(event.getUserId(), new PointCreateRequest(event.getObtainPoint(),
+                ORDER_OBTAIN_CONTENT + event.getOrderId(), OBTAIN
+        ));
+
+    }
 
     @Override
     @Transactional(readOnly = true)
@@ -42,7 +73,7 @@ public class PointServiceImpl implements PointService{
 
     @Override
     public Page<PointResponse> searchCondition(PointSearchCondition condition, int page) {
-        if(page < 0){
+        if (page < 0) {
             page = 0;
         }
         return pointRepository.searchPageCondition(condition, PageRequest.of(page, POINT_SIZE))
